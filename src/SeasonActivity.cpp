cffa0e8cc2fe0b958a7e150a5322057566eba601
hanc2720 <colby.hanna@uleth.ca>
2024-11-30 16:45:41 -0700

    implemented activites for Fletclings season

---
+++ src/SeasonActivity.cpp
@@ -0,0 +1,17 @@
+#include "SeasonActivity.hpp"
+
+void FletchlingSpring::performActivity() {
+    cout << "Fletchling Spring Activity\n"
+}
+
+void FletchlingSummer::performActivity() {
+    cout << "Fletchling Summer Activity\n"
+}
+
+void FletchlingFall::performActivity() {
+    cout << "Fletchling Fall Activity\n"
+}
+
+void FletchlingWinter::performActivity() {
+    cout << "Fletchling Winter Activity\n"
+}
