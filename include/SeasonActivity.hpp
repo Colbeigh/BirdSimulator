e22f93159b0436622b053d6fc65078be16611f26
hanc2720 <colby.hanna@uleth.ca>
2024-11-30 16:35:30 -0700

    git add include/SeasonActivity.hpp

--- include/SeasonActivity.hpp
+++ include/SeasonActivity.hpp
@@ -10,4 +10,32 @@ public:
  virtual performActivity() = 0;
 }
 
+class FletchlingSpring {
+public:
+ virtual performActivity(){
+    std:: cout << ""
+ }
+}
+
+class FletchlingSummer {
+public:
+ virtual performActivity(){
+    std:: cout << ""
+ }
+}
+
+class FletchlingFall {
+public:
+ virtual performActivity(){
+    std:: cout << ""
+ }
+}
+
+class FletchlingWinter {
+public:
+ virtual performActivity(){
+    std:: cout << ""
+ }
+}
+
 #endif
