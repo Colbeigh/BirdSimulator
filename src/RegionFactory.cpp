9cc2d4753548ecc9aa9f1e1e86cc1c079b132616
hanc2720 <colby.hanna@uleth.ca>
2024-11-30 02:55:12 -0700

    Implemented CrearteBird function for Unova

--- src/RegionFactory.cpp
+++ src/RegionFactory.cpp
@@ -5,3 +5,9 @@ std::vector<bird*> KalosRegionFactory::createBird{
     birds.push_back(new Fletchling())
     returns birds;
 }
+
+std::vector<bird*> UnovaRegionFactory::createBird{
+    std::vector<Bird*> birds;
+    birds.push_back(new Rufflet())
+    returns birds;
+}
