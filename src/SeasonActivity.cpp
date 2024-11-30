0c39bb913a74a794c1e3b95cea854cdb09eaa6d0
hanc2720 <colby.hanna@uleth.ca>
2024-11-30 16:47:13 -0700

    added missing std:: and semicolons

--- src/SeasonActivity.cpp
+++ src/SeasonActivity.cpp
@@ -1,17 +1,17 @@
 #include "SeasonActivity.hpp"
 
 void FletchlingSpring::performActivity() {
-    std::cout << "Fletchling Spring Activity\n"
+    std::cout << "Fletchling Spring Activity\n";
 }
 
 void FletchlingSummer::performActivity() {
-    std::cout << "Fletchling Summer Activity\n"
+    std::cout << "Fletchling Summer Activity\n";
 }
 
 void FletchlingFall::performActivity() {
-    std::cout << "Fletchling Fall Activity\n"
+    std::cout << "Fletchling Fall Activity\n";
 }
 
 void FletchlingWinter::performActivity() {
-    std::cout << "Fletchling Winter Activity\n"
+    std::cout << "Fletchling Winter Activity\n";
 }
