677e04a3adf202a5cddfbceba0d03b306cedc283
hanc2720 <colby.hanna@uleth.ca>
2024-11-30 22:52:01 -0700

    make it print the season

--- src/SeasonActivity.cpp
+++ src/SeasonActivity.cpp
@@ -12,36 +12,42 @@ void FletchlingSpring::performActivity(std::string name) {
 }
 
 void FletchlingSummer::performActivity(std::string name) {
+    std::cout << "\nSeason: Summer\n";
     std::cout << "Fletchling Summer Activity\n";
 }
 
 void FletchlingFall::performActivity(std::string name) {
+    std::cout << "\nSeason: Fall\n";
     std::cout << "Fletchling Fall Activity\n";
 }
 
 void FletchlingWinter::performActivity(std::string name) {
+    std::cout << "\nSeason: Winter\n";
     std::cout << "Fletchling Winter Activity\n";
 }
 
-RuffletSpring::RuffletSpring() {
-    std::cout << "\nSeason: Spring\n"; 
+RuffletSpring::RuffletSpring() { 
     nestbuilder = new RuffletNestBuilder;
     nestbuilder ->buildNest(nest);
 }
 
 void RuffletSpring::performActivity(std::string name) {
+    std::cout << "\nSeason: Spring\n";
     std::cout << "Rufflet Spring Activity\n";
     nest.printNest();
 }
 
 void RuffletSummer::performActivity(std::string name) {
+    std::cout << "\nSeason: Summer\n";
     std::cout << "Rufflet Summer Activity\n";
 }
 
 void RuffletFall::performActivity(std::string name) {
+    std::cout << "\nSeason: Fallr\n";
     std::cout << "Ruffletg Fall Activity\n";
 }
 
 void RuffletWinter::performActivity(std::string name) {
+    std::cout << "\nSeason: Winter\n";
     std::cout << "Rufflet Winter Activity\n";
 }
