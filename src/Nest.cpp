b4467b3159e78a097828bef82595f2ae5fd4e708
hanc2720 <colby.hanna@uleth.ca>
2024-12-01 03:36:01 -0700

    made a change and reverted

--- src/Nest.cpp
+++ src/Nest.cpp
@@ -1,18 +1,18 @@
 #include "Nest.hpp"
 
-void Nest::addMaterial(std::string material) {
+void Nest::addMaterial(std::string material){
     materials.push_back(material);
 }
 
-void Nest::printNest() {
+void Nest::printNest(){
     if (materials.empty()) {
         std::cout << "There is no nest\n";
         return;
     }
 
-    std::cout << "The Nest is built of: ";
+    std::cout << "The nest is built of: ";
     for (int i = 0; i < materials.size(); i++) {
-        if (i > 0 && i== materials.size() -1) {
+        if(i > 0 && i== materials.size() -1) {
             std::cout << " and ";
         } else if (i > 0) {
             std::cout << ", ";
