ee8f18b2f7edfa4ef8767cf2eb91780ecc250bc8
hanc2720 <colby.hanna@uleth.ca>
2024-12-01 03:38:34 -0700

    Fixed style check errors

--- src/Nest.cpp
+++ src/Nest.cpp
@@ -1,10 +1,10 @@
 #include "Nest.hpp"
 
-void Nest::addMaterial(std::string material){
+void Nest::addMaterial(std::string material) {
     materials.push_back(material);
 }
 
-void Nest::printNest(){
+void Nest::printNest() {
     if (materials.empty()) {
         std::cout << "There is no nest\n";
         return;
@@ -12,7 +12,7 @@ void Nest::printNest(){
 
     std::cout << "The nest is built of: ";
     for (int i = 0; i < materials.size(); i++) {
-        if(i > 0 && i== materials.size() -1) {
+        if (i > 0 && i== materials.size() -1) {
             std::cout << " and ";
         } else if (i > 0) {
             std::cout << ", ";
