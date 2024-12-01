e1e2e429c20d0e2dac0177ac2ca73c938ae9dbb7
hanc2720 <colby.hanna@uleth.ca>
2024-11-30 23:54:55 -0700

    added new birds Pikipek and Starly into functionality

--- src/RegionFactory.cpp
+++ src/RegionFactory.cpp
@@ -3,11 +3,13 @@
 std::vector<Bird*> KalosRegionFactory::createBird() {
     std::vector<Bird*> birds;
     birds.push_back(new Fletchling());
+    birds.push_back(new PikiPek());
     return birds;
 }
 
 std::vector<Bird*> UnovaRegionFactory::createBird() {
     std::vector<Bird*> birds;
     birds.push_back(new Rufflet());
+    birds.push_back(new Starly());
     return birds;
 }
