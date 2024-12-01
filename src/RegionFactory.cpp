7a632e8677f4fe83914511ca6f4e8a64c41d5eb3
hanc2720 <colby.hanna@uleth.ca>
2024-12-01 04:29:12 -0700

    added destructor

--- src/RegionFactory.cpp
+++ src/RegionFactory.cpp
@@ -1,14 +1,24 @@
 #include "RegionFactory.hpp"
 
+KalosRegionFactory::~KalosRegionFactory() {
+    for (Bird* bird : birds) {
+    delete bird;
+    }
+}
+
 std::vector<Bird*> KalosRegionFactory::createBird() {
-    std::vector<Bird*> birds;
     birds.push_back(new Fletchling());
     birds.push_back(new PikiPek());
     return birds;
 }
 
+UnovaRegionFactory::~UnovaRegionFactory() {
+    for (Bird* bird : birds) {
+    delete bird;
+    }
+}
+
 std::vector<Bird*> UnovaRegionFactory::createBird() {
-    std::vector<Bird*> birds;
     birds.push_back(new Rufflet());
     birds.push_back(new Starly());
     return birds;
