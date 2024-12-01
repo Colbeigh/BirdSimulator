50291d9fb28cdfeae41db8178c69c4afa9223e4b
hanc2720 <colby.hanna@uleth.ca>
2024-11-30 17:32:49 -0700

    added missing () and missing semicolon

--- src/RegionFactory.cpp
+++ src/RegionFactory.cpp
@@ -1,13 +1,13 @@
 #include "RegionFactory.hpp"
 
-std::vector<bird*> KalosRegionFactory::createBird{
+std::vector<Bird*> KalosRegionFactory::createBird() {
     std::vector<Bird*> birds;
-    birds.push_back(new Fletchling())
-    returns birds;
+    birds.push_back(new Fletchling());
+    return birds;
 }
 
-std::vector<bird*> UnovaRegionFactory::createBird{
+std::vector<Bird*> UnovaRegionFactory::createBird() {
     std::vector<Bird*> birds;
-    birds.push_back(new Rufflet())
-    returns birds;
+    birds.push_back(new Rufflet());
+    return birds;
 }
