46dcf503f1de170484d73f201b612f4f87532408
hanc2720 <colby.hanna@uleth.ca>
2024-12-02 20:50:03 -0700

    Removed bool to check if its running and got rid of stlye errors

--- src/birds/main.cpp
+++ src/birds/main.cpp
@@ -4,9 +4,8 @@
 int main() {
     Simulation simulation;
     int userinput;
-    bool exit = false;
 
-    while (!exit) {
+    while (true) {
         std::cout << "Please pick a region)\n";
         std::cout << "1.) Kalos\n";
         std::cout << "2.) Unova\n";
@@ -18,12 +17,12 @@ int main() {
             std::cout << "Invalid input. Please enter an" <<
             "integer between 1 and 3\n";
             continue;
-        } 
+        }
         if (userinput < 1 || userinput > 3) {
            std::cout << "Invalid input. Please enter an" <<
            "integer between 1 and 3\n";
            continue;
-        } 
+        }
         switch (userinput) {
             case 1:
             case 2:
@@ -32,9 +31,8 @@ int main() {
                 break;
             case 3:
                 std::cout << "Exiting\n";
-                exit = true;
-
+                return 0;
         }
-        return 0;
     }
+    return 0;
 }
